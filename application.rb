3887721ef37b43b13d39abfbfca315328c961f6d
github cjl248 <github cjl248@cornell.edu>
2019-08-03 21:24:57 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,11 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is"
+    #resp.write "<h1> Hello, World </h1>"
+    #resp.write "<p> <b> hello <i> world </i> </b> </p>"
+    #resp.write "<body> This Chris from accross the interwebs! </body>"
     resp.finish
   end
 
 end
-
